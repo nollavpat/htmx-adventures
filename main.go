193a2cb3c2b9b6cca1145f7bce9d75cbed43e143
Jairus Patrick Vallon <jairus.vallon@angkas.com>
2024-07-28 15:15:43 +0800

    Complete example

--- main.go
+++ main.go
@@ -1,16 +1,38 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/nollavpat/chat-htmx/handler"
+)
 
 func main() {
+	// Create views engine
+	viewsEngine := html.New("./views", ".html")
 
 	// Start new fiber instance
-	app := fiber.New()
+	app := fiber.New(fiber.Config{Views: viewsEngine})
+
+	// Static route and directory
+	app.Static("/static/", "./static")
 
 	// Create a "ping" handler to test the server
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Welcome to fiber")
 	})
+
+	appHandler := handler.NewAppHandler()
+
+	app.Get("/", appHandler.HandleGetIndex)
+
+	// create new webscoket
+	server := NewWebSocket()
+	app.Get("/ws", websocket.New(func(ctx *websocket.Conn) {
+		server.HandleWebSocket(ctx)
+	}))
+
+	go server.HandleMessages()
 
 	// Start the http server
 	app.Listen("localhost:3000")
